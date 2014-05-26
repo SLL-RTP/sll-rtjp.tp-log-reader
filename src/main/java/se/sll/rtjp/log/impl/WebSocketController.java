c34764ba08a0d640fbfbe20c8b6f78d794399961
Peter Larsson <peter.larsson@callistaenterprise.se>
2014-05-26 18:52:47 +0200

    Import statements has been cleaned-up.

--- src/main/java/se/sll/rtjp/log/impl/WebSocketController.java
+++ src/main/java/se/sll/rtjp/log/impl/WebSocketController.java
@@ -1,10 +1,8 @@
 package se.sll.rtjp.log.impl;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import se.sll.rtjp.log.api.Status;
 
